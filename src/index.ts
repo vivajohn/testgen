843594a8e6305c8f68986dc18e435a21a1c0467e
John Glowinski <hanscdn@gmail.com>
2020-08-12 13:10:43 +0200

    Updated input path

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import { Parser } from './parser/tsparser';
 
 console.log('START');
 
-const filer = new Filer('C:/tests/castclientcopy/src/app');
-// const filer = new Filer('C:/Projects/testgen2/testinput');
+// const filer = new Filer('C:/tests/castclientcopy/src/app');
+const filer = new Filer('./testinput/src');
 const parser = new Parser();
 parser.run(filer);
 
